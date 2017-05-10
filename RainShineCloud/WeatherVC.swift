71255fa512c2580ad6cb5ece91592eaab80bf418
Scott <rawradiofl@gmail.com>
2017-05-09 20:31:47 -0400

    Adds basic UI

--- RainShineCloud/WeatherVC.swift
+++ RainShineCloud/WeatherVC.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentConditionsImage: UIImageView!
+    @IBOutlet weak var currentConditionsLabel: UILabel!
+    @IBOutlet weak var forcastTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        forcastTableView.delegate = self
+        forcastTableView.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Day Cell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+    
+    
+    
 
 }
 
